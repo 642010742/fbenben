7f62e011d3b93682f98742fa0f2348cf1cf79192
zmf <1109396289@qq.com>
2020-09-04 17:17:40 +0800

    增加弹框

--- app/src/main/java/com/dwz/mvvmdemo/ui/fragment/TestFragment.java
+++ app/src/main/java/com/dwz/mvvmdemo/ui/fragment/TestFragment.java
@@ -1,7 +1,13 @@
 package com.dwz.mvvmdemo.ui.fragment;
 
+import android.view.View;
+import android.widget.Toast;
+
 import com.dwz.mvvmdemo.R;
-import com.dwz.mvvmdemo.vm.MainVModel;
+import com.dwz.mvvmdemo.vm.TestFragmentVmodel;
+import com.qmuiteam.qmui.skin.QMUISkinManager;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialogAction;
 
 import library.baseView.BaseFragment;
 
@@ -11,8 +17,9 @@ import library.baseView.BaseFragment;
  * @describe
  * @DWZ
  */
-public class TestFragment extends BaseFragment<MainVModel> {
-
+public class TestFragment extends BaseFragment<TestFragmentVmodel> implements View.OnClickListener {
+    private int mCurrentDialogStyle = com.qmuiteam.qmui.R.style.QMUI_Dialog;
+//    private int mCurrentDialogStyle = R.style.DialogTheme2;
 
     @Override
     protected int getLayoutID() {
@@ -21,11 +28,80 @@ public class TestFragment extends BaseFragment<MainVModel> {
 
     @Override
     protected Class getVModelClass() {
-        return MainVModel.class;
+        return TestFragmentVmodel.class;
     }
 
     @Override
     public void initView() {
+        vm.bind.changeAlpha.setChangeAlphaWhenPress(true);
 
+        initListener();
+    }
+
+    public void initListener() {
+        vm.bind.showDialog1.setOnClickListener(this);
+        vm.bind.showDialog2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.showDialog1:
+                showMessagePositiveDialog();
+                break;
+            case R.id.showDialog2:
+                showLongMessageDialog();
+                break;
+        }
+
+    }
+
+    // ================================ 生成不同类型的对话框
+    private void showMessagePositiveDialog() {
+        new QMUIDialog.MessageDialogBuilder(getActivity())
+                .setTitle("标题")
+                .setMessage("确定要发送吗？")
+                .setSkinManager(QMUISkinManager.defaultInstance(getContext()))
+                .addAction("取消", new QMUIDialogAction.ActionListener() {
+                    @Override
+                    public void onClick(QMUIDialog dialog, int index) {
+                        dialog.dismiss();
+                    }
+                })
+                //QMUIDialogAction.ACTION_PROP_POSITIVE  蓝色
+                //QMUIDialogAction.ACTION_PROP_NEGATIVE  红色
+                //QMUIDialogAction.ACTION_PROP_NEUTRAL  蓝色
+                .addAction(0, "确定", QMUIDialogAction.ACTION_PROP_NEUTRAL, new QMUIDialogAction.ActionListener() {
+                    @Override
+                    public void onClick(QMUIDialog dialog, int index) {
+                        dialog.dismiss();
+                        Toast.makeText(getActivity(), "发送成功", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .create(mCurrentDialogStyle).show();
+    }
+
+    private void showLongMessageDialog() {
+        new QMUIDialog.MessageDialogBuilder(getActivity())
+                .setTitle("标题")
+                .setSkinManager(QMUISkinManager.defaultInstance(getContext()))
+                .setMessage("这是一段很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长" +
+                        "很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很" +
+                        "很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很" +
+                        "很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很" +
+                        "长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长" +
+                        "很长很长很长很长很很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长" +
+                        "很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长" +
+                        "很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长" +
+                        "很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长" +
+                        "很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长" +
+                        "很长很长很长很长很长很长很长很长很长很长很长很长很长很长长很长的文案")
+                .addAction("取消", new QMUIDialogAction.ActionListener() {
+                    @Override
+                    public void onClick(QMUIDialog dialog, int index) {
+                        dialog.dismiss();
+                    }
+                })
+                .create(mCurrentDialogStyle).show();
     }
 }
---
+++ app/src/main/java/com/dwz/mvvmdemo/vm/TestFragmentVmodel.java
@@ -0,0 +1,12 @@
+package com.dwz.mvvmdemo.vm;
+
+import com.dwz.mvvmdemo.databinding.FragmentTestBinding;
+
+import library.baseVModel.BaseVModel;
+
+public class TestFragmentVmodel extends BaseVModel<FragmentTestBinding> {
+    @Override
+    public void initPresenter() {
+
+    }
+}
